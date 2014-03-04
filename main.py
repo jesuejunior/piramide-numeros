349be7ac763bb1f6edc8b822b81de182162be776
Jesue Junior <jesuesousa@gmail.com>
2014-03-04 19:29:50 -0300

    Quase certo, mais ainda quebra um teste'

--- main.py
+++ main.py
@@ -5,73 +5,26 @@ import re
 import time
 
 
-def get_bigger_neighbor(array, last_index=None):
-
-    if last_index:
+def get_bigger_neighbor(array, last_index=0):
+    if len(array) == 1:
         value = max(array)
         index = last_index
         return value, index
     else:
-        value = max(array)
+        # value = max(array)
+        index = last_index
+
+        start = 1
+        end = index + 2
+        if len(array) <= 2:
+            start = 0
+            end = index + 1
+
+        tmp_array = array[slice(start,end)]
+        value = max(tmp_array)
         index = array.index(value)
 
-    start = index-1
-    end = index + 2
-    if len(array) < 3:
-        start = 0
-        end = index + 1
-
-    tmp_array = array[slice(start,end)]
-    value = max(tmp_array)
-    index = array.index(value)
-
-    return value, index
-
-
-class Queue(object): 
-
-    def __init__(self):
-        self.holder = []
-
-    def enqueue(self,val):
-        self.holder.append(val)
-
-    def dequeue(self):
-        val = None
-        try:
-            val = self.holder[0]
-            if len(self.holder) == 1:
-                self.holder = []
-            else:
-                self.holder = self.holder[1:]
-        except:
-            pass
-        return val
-
-    def is_empty(self):
-        result = False
-        if len(self.holder) == 0:
-            result = True
-        return result
-
-
-def bfs(graph,start,end,q):
-
-    temp_path = [start]
-    q.enqueue(temp_path)
-
-    while q.is_empty() == False:
-        tmp_path = q.dequeue()
-        last_node = tmp_path[len(tmp_path)-1]
-        print tmp_path
-        if last_node == end:
-            print "VALID_PATH : ",tmp_path
-        for link_node in graph[last_node]:
-            if link_node not in tmp_path:
-                new_path = []
-                new_path = tmp_path + [link_node]
-                q.enqueue(new_path)
-
+        return value, index
 
 def clear_and_convert_to_int(line):
     line = re.findall("\d+" , line)
@@ -85,19 +38,11 @@ def execute():
         nodes = []
         for line in fileinput.input():
             nodes.append(clear_and_convert_to_int(line))
-
-        print nodes
         total = 0
         index = 0
-        value = 0
         for n in nodes:
-            if not index:
-                value, index = get_bigger_neighbor(n,last_index=0)
-            else:
-                value, index = get_bigger_neighbor(n,last_index=index)
-
+            value, index = get_bigger_neighbor(n,last_index=index)
             total += value
-
         print total
 if __name__ == "__main__":
 
